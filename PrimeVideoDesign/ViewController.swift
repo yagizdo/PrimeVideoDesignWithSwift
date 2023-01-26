5b26e67b9042deb1f57312097d0c4e99fb2e68d1
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2023-01-26 14:18:11 +0300

    Initial Commit

---
+++ PrimeVideoDesign/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PrimeVideoDesign
+//
+//  Created by Yılmaz Yağız Dokumacı on 26.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
