e3609fd10e453e4b0d36ededf7824511dede5bf8
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2023-01-28 22:00:17 +0300

    Added image slider for home tab

---
+++ PrimeVideoDesign/ImageSliderCellCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImageSliderCellCollectionViewCell.swift
+//  PrimeVideoDesign
+//
+//  Created by Yılmaz Yağız Dokumacı on 28.01.2023.
+//
+
+import UIKit
+
+class ImageSliderCellCollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var sliderImageView: UIImageView!
+}
--- PrimeVideoDesign/TabControllers/HomeTabViewController.swift
+++ PrimeVideoDesign/TabControllers/HomeTabViewController.swift
@@ -9,21 +9,58 @@ import UIKit
 
 class HomeTabViewController: UIViewController {
 
+    
+    @IBOutlet weak var imageSliderCollectionView: UICollectionView!
+    
+    
+    @IBOutlet weak var pageDots: UIPageControl!
+    
+    var sliderImages = ["img1","img1","img1","img1","img1"]
+    var imageIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        imageSliderCollectionView.delegate = self
+        imageSliderCollectionView.dataSource = self
+        
+        pageDots.numberOfPages = sliderImages.count
+        pageDots.currentPage = imageIndex
+        
+        Timer.scheduledTimer(timeInterval: 7.0, target: self, selector: #selector(scrollingSetup), userInfo: nil, repeats: true)
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func scrollingSetup() {
+        
+        if imageIndex < sliderImages.count - 1 {
+            imageIndex += 1
+        } else {
+            imageIndex = 0
+        }
+        
+        pageDots.numberOfPages = sliderImages.count
+        pageDots.currentPage = imageIndex
+        imageSliderCollectionView.scrollToItem(at: IndexPath(item: imageIndex, section: 0), at: .right, animated: true)
     }
-    */
 
+}
+
+extension HomeTabViewController : UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return sliderImages.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = imageSliderCollectionView.dequeueReusableCell(withReuseIdentifier: "imageSliderCell", for: indexPath) as! ImageSliderCellCollectionViewCell
+        
+        cell.sliderImageView.image = UIImage(named: sliderImages[indexPath.row])
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
+    }
 }
