af33a1ef000486c06f4fac2bb5d69fd49ff8357b
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2023-01-29 17:52:33 +0300

    Added continue watching list

---
+++ PrimeVideoDesign/ContinueWatchCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ContinueWatchCollectionViewCell.swift
+//  PrimeVideoDesign
+//
+//  Created by Yılmaz Yağız Dokumacı on 29.01.2023.
+//
+
+import UIKit
+
+class ContinueWatchCollectionViewCell: UICollectionViewCell {
+    
+    
+    
+    @IBOutlet weak var continueWatchImage: UIImageView!
+}
--- PrimeVideoDesign/TabControllers/HomeTabViewController.swift
+++ PrimeVideoDesign/TabControllers/HomeTabViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class HomeTabViewController: UIViewController {
 
+    @IBOutlet weak var continueWatchCollectionView: UICollectionView!
     
     @IBOutlet weak var imageSliderCollectionView: UICollectionView!
     
@@ -16,6 +17,7 @@ class HomeTabViewController: UIViewController {
     @IBOutlet weak var pageDots: UIPageControl!
     
     var sliderImages = ["familyguyslider","bobsburgersslider","theboysslider","jackryanslider"]
+    var continueWatchThumbnails = ["jackryanThumbnail","thetestThumbnail","wildcatThumbnail","thetestThumbnail","wildcatThumbnail"]
     var imageIndex = 0
     
     override func viewDidLoad() {
@@ -25,21 +27,11 @@ class HomeTabViewController: UIViewController {
         imageSliderCollectionView.dataSource = self
         imageSliderCollectionView.isPagingEnabled = true
         
-        let cellDesign = UICollectionViewFlowLayout()
+        continueWatchCollectionView.dataSource = self
+        continueWatchCollectionView.delegate = self
         
-        cellDesign.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-        
-        cellDesign.minimumLineSpacing = 0
-        cellDesign.minimumInteritemSpacing = 0
-        cellDesign.scrollDirection = .horizontal
-        
-        let screenWidth = UIScreen.main.bounds.width
-        
-        let cellWidth = (screenWidth) / 1
-        
-        cellDesign.itemSize = CGSize(width: cellWidth, height: cellWidth)
-        
-        imageSliderCollectionView.collectionViewLayout = cellDesign
+        updateImageSliderCellLayout()
+        updatecontinueWatchCellLayout()
         
         pageDots.numberOfPages = sliderImages.count
         pageDots.currentPage = imageIndex
@@ -60,25 +52,79 @@ class HomeTabViewController: UIViewController {
         pageDots.currentPage = imageIndex
         imageSliderCollectionView.scrollToItem(at: IndexPath(item: imageIndex, section: 0), at: .right, animated: true)
     }
+    
+    func updateImageSliderCellLayout() {
+        let cellDesign = UICollectionViewFlowLayout()
+        
+        cellDesign.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        
+        cellDesign.minimumLineSpacing = 0
+        cellDesign.minimumInteritemSpacing = 0
+        cellDesign.scrollDirection = .horizontal
+
+        imageSliderCollectionView.collectionViewLayout = cellDesign
+    }
+    
+    func updatecontinueWatchCellLayout() {
+        let cellDesign = UICollectionViewFlowLayout()
+        
+        cellDesign.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        
+        cellDesign.minimumLineSpacing = 15
+        cellDesign.minimumInteritemSpacing = 0
+        cellDesign.scrollDirection = .horizontal
+        
+        let screenWidth = UIScreen.main.bounds.width
+        
+        let cellWidth = (screenWidth) / 1
+        
+        cellDesign.itemSize = CGSize(width: 174, height: 100)
+        
+        
+        
+        continueWatchCollectionView.collectionViewLayout = cellDesign
+    }
 
 }
 
 extension HomeTabViewController : UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return sliderImages.count
+        if collectionView == imageSliderCollectionView {
+            return sliderImages.count
+        } else if collectionView == continueWatchCollectionView {
+            return 5
+        }
+        
+        return 0
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = imageSliderCollectionView.dequeueReusableCell(withReuseIdentifier: "imageSliderCell", for: indexPath) as! ImageSliderCellCollectionViewCell
+        if collectionView == imageSliderCollectionView {
+            let cell = imageSliderCollectionView.dequeueReusableCell(withReuseIdentifier: "imageSliderCell", for: indexPath) as! ImageSliderCellCollectionViewCell
+            
+            cell.sliderImageView.image = UIImage(named: sliderImages[indexPath.row])
+            
+            return cell
+        } else if collectionView == continueWatchCollectionView {
+            let cell = continueWatchCollectionView.dequeueReusableCell(withReuseIdentifier: "continueWatchCell", for: indexPath) as! ContinueWatchCollectionViewCell
+            
+            cell.continueWatchImage.image = UIImage(named: continueWatchThumbnails[indexPath.row])
+            return cell
+        }
         
-        cell.sliderImageView.image = UIImage(named: sliderImages[indexPath.row])
-        
-        return cell
+        return UICollectionViewCell()
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
+        
+        if collectionView == imageSliderCollectionView {
+            return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
+        } else if collectionView == continueWatchCollectionView {
+            return CGSize(width: 174, height: 100)
+        }
+        
+        return CGSize.zero
     }
     
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
